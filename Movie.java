a6ed506d557ba563dd290f6667ba0284e6da2042
Luke Tomassetti <luke>
2026-09-21 17:08:04 +0200

    removed ratings and language

--- Movie.java
+++ Movie.java
@@ -5,17 +5,15 @@ public class Movie {
     private int year;
     private int length;
     private String genre;
-    private String rating;
-    private String language;
+    
 
-    public Movie(String title, String director, int year, int length, String genre, String rating, String language) {
+    public Movie(String title, String director, int year, int length, String genre) {
         this.title = title;
         this.director = director;
         this.year = year;
         this.length = length;
         this.genre = genre;
-        this.rating = rating;
-        this.language = language;
+        
     }
     
 
@@ -59,23 +57,5 @@ public class Movie {
 
     public void setGenre(String genre) {
         this.genre = genre;}
-
-
-
-
-    public String getRating() { 
-        return rating;}
-
-
-    public void setRating(String rating) {
-        this.rating = rating;}
-
-
-        
-    public String getLanguage() {
-        return language;}
-
-    public void setLanguage(String language) {
-        this.language = language;}
 
 }
