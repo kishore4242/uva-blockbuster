e93eaf1b828f86fcf06a9e55ecc3ee6ab260cc90
Luke Tomassetti <luke>
2026-09-23 16:24:46 +0200

    added id

--- customer/Customer.java
+++ customer/Customer.java
@@ -3,10 +3,12 @@ package customer;
 public class Customer {
 private String name;
 private String email;
+private final int id;
 
-public Customer(String name, String email) {
+public Customer(String name, String email, int id) {
     setName(name);
     setEmail(email);
+    this.id = id;
 }
 public void setName(String name) {
     this.name = name;
@@ -21,4 +23,7 @@ public String getEmail() {
     return email;
 }
 
+public int getId() {
+    return id;
+}
 }
